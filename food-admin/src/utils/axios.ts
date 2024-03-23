6e6a26ba3bb7d833a07a5a0ae63fbb1a47922ff0
Zoya <naomibeck0105@gmail.com>
2024-03-23 11:16:37 +0800

    axios url uurchluw

--- food-admin/src/utils/axios.ts
+++ food-admin/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-const BASE_API_URL = "http://localhost:8080/";
+const BASE_API_URL = "https://food-fullstack-app.vercel.app/";
 
 const instanceAxios = axios.create({
   baseURL: BASE_API_URL,
